f5416bfbd68c3f56bdc3045d0fe08fb041ac013b
NaufalID <88891630+NaufalID@users.noreply.github.com>
2022-10-02 01:29:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6289601124991']
+global.ownernomer = "6289601124991"
+global.premium = ['6289601124991']
+global.packname = 'Whatsapp-Botz'
+global.author = 'Naufal Muflih'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
